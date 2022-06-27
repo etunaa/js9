4596f8fe1860b3e03fcc443ca892cc8d53241474
etunaa <104025100+etunaa@users.noreply.github.com>
2022-06-27 21:20:07 +0200

    2

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,37 @@
-alert (ola)
+document.querySelectorAll(".carousel").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHtml = Array.from(items, () => {
+      return `<span class="carousel__button"></span>`;
+    });
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="carousel__nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".carousel__button");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+       
+        items.forEach(item =>
+          item.classList.remove("carousel__item__selected")
+        );
+        buttons.forEach(button =>
+          button.classList.remove("carousel__button__selected")
+        );
+  
+        items[i].classList.add("carousel__item__selected");
+        button.classList.add("carousel__button__selected");
+      });
+    });
+  
+ 
+    items[0].classList.add("carousel__item__selected");
+    buttons[0].classList.add("carousel__button__selected");
+  });
+  
